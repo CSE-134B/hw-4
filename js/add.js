caf72fa200d3590d08307d87c6e896a49d5db383
Tyler Tedeschi <ttedesch@ucsd.edu>
2015-11-07 20:21:49 -0800

    Added base code for saving a new habit

--- js/add.js
+++ js/add.js
@@ -1,3 +1,14 @@
 var firebaseRef = new Firebase('boilding-torch-2236.firebaseIO.com');
 
+//Get a reference to the habits part of the database
+var habitsRef = ref.child("habits");
 
+//Save the object to the database
+// habitsRef.set({
+// 	habittitle: {
+// 		icon: 				"icon.png",
+// 		frequency: 			"M,W,T",
+// 		daily_frequency: 	"3",
+// 		others: 			"value"  
+// 	}
+// });
