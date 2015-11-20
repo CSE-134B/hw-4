5da3e001083545123c0071c9b0d9f7adc88d91f0
Tyler Tedeschi <ttedesch@ucsd.edu>
2015-11-20 12:53:26 -0800

    Delete habit deletes notifications

--- js/list.js
+++ js/list.js
@@ -14,24 +14,11 @@ function showMsg(element){
           
 
 
-var animator = function(oProgress){
-	var iMsecsPerUpdate = 1000/60; 	// # of milliseconds between updates, this gives 60fps
-	var iDuration = 3;             	// secs to animate for
-	var iInterval = oProgress.getAttribute('max')/(iDuration*1000/iMsecsPerUpdate);	//Edit this to change the amount the bar animates for
-    oProgress.value = oProgress.value + iInterval;
-    if (oProgress.value + iInterval < oProgress.getAttribute('max')){
-       setTimeout(animator(oProgress), iMsecsPerUpdate);
-    } else {
-        oProgress.value = oProgress.getAttribute('max');
-    }
-}
+
 
 var progressBarAnimate = function(doneButton){
     console.log("entered function op-done");
-	var oParentLi = doneButton.parentNode.parentNode;
-	var aChildren = oParentLi.getElementsByTagName('progress');
-	var oProgress = aChildren[0];
-    animator(oProgress);
+	
 }
 
 document.querySelector(".op-del").onclick = function(){
